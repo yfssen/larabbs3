4fdb1aeb2316254a634e36e75a795bf090c11044
yfs <yfssen@126.com>
2019-08-22 11:51:44 +0000

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 class UsersController extends Controller
 {
     //
@@ -21,10 +22,17 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request,User $user)
+    public function update(UserRequest $request,ImageUploadHandler  $uploader,User $user)
     {
+        $data = $request->all();
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            if ($result) {
+                $data['avatar']=$result['path'];
 
-        $user->update($request->all());
+            }
+        }
+        $res=$user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
