25552e206176598da5c0253efd74e3778642e2b4
yfs <yfssen@126.com>
2019-08-23 01:03:50 +0000

    填充用户和话题数据

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,16 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+    $sentence = $faker->sentence();
+    //随机取一个月内的事件
+    $updated_at = $faker->dateTimeThisMonth();
+    $created_at = $faker->dateTimeThisMonth($updated_at);
     return [
+        'title'=>$sentence,
+        'body'=>$faker->text,
+        'excerpt' => $sentence,
+        'created_at' => $created_at,
+        'updated_at'=>$updated_at,
         // 'name' => $faker->name,
     ];
 });
